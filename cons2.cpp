b8e48276325ae051ea3a73ed368669cf8fdc3210
panjiprann <panjipranantya20@gmail.com>
2024-05-14 15:00:27 +0700

    membuat static

--- cons2.cpp
+++ cons2.cpp
@@ -5,4 +5,15 @@ using namespace std;
 class mahasiswa
 {
     private:
+        static int nim;
+
+    public:
+        int id;
+        string nama;
+
+        void setID();
+        void printALL();
+
+        static void setNim(int pNim) { nim = pNim;}
+        static int getNim() { return nim; }
 }
