7915f6bc9ccc5104ac4d5f330c0cd37babcc7025
panjiprann <panjipranantya20@gmail.com>
2024-05-14 18:26:54 +0700

    menyelesaikan class buku

--- cfunction.cpp
+++ cfunction.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 class buku
 {
-    
-}
+    string judul;
+
+public:
+    buku setJudul(string judul)
+    {
+        this->judul = judul;
+        return *this; //chaun function
+    }
+    string getJudul()
+    {
+        return this->judul;
+    }
+} bukunya;
