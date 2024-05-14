e20fd20a9d17e43e689dd9784cc2f3fff08053f2
panjiprann <panjipranantya20@gmail.com>
2024-05-14 15:33:16 +0700

    menyelesaikan class mahasiswa

---
+++ cons3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa
+{
+    public:
+        int nim;
+        string nama;
+
+    public:
+        Mahasiswa()
+        {
+            nim = 0;
+            nama = "";
+        };
+
+        Mahasiswa(int iNim)
+        {
+            nim = iNim;
+        };
+        Mahasiswa(int iNim, string iNama)
+        {
+            nim = iNim;
+            nama = iNama;
+        };
+        void cetak(){
+            cout << "NIM : " << nim << endl;
+            cout << "Nama : " << nama << endl;
+            cout << endl;
+        };
+};
